b28a19fe9aa41768770168f5fd81b753ed259d18
Venkat Nikhil Kotagiri <kvns29991@gmail.com>
2026-03-04 04:45:14 +0000

    Added root url response

--- service/routes.py
+++ service/routes.py
@@ -34,7 +34,11 @@ from service.common import status  # HTTP Status Codes
 def index():
     """Root URL response"""
     return (
-        "Reminder: return some useful information in json format about the service here",
+        jsonify(
+            name="Shopcarts REST API Service",
+            version="1.0",
+            paths=url_for("list_shopcarts", _external=True),
+        ),
         status.HTTP_200_OK,
     )
 
