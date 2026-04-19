c704f162afed13bd147e78fdc6071eba52fc3e98
goathe2001 <59945290+goathe2001@users.noreply.github.com>
2026-04-19 11:11:08 -0400

    Add files via upload

--- tests/test_swagger.py
+++ tests/test_swagger.py
@@ -1,3 +1,18 @@
+######################################################################
+# Copyright 2016, 2026
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+######################################################################
 
 """
 Test cases for verifying Swagger/OpenAPI integration
@@ -40,7 +55,8 @@ class TestSwagger(unittest.TestCase):
             self.assertEqual(
                 response.status_code,
                 200,
-                "Expected 200 OK response for /apidocs/ but received %s" % response.status_code,
+                # Use f-string for clarity and to satisfy pylint C0209
+                f"Expected 200 OK response for /apidocs/ but received {response.status_code}",
             )
 
 
